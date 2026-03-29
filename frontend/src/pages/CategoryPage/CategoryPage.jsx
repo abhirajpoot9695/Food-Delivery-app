6d448a1c54aa60158f7fd5d1be9e21818a8667da
Abhishek Rajpoot <abhirajpoot9695@gmail.com>
2026-03-29 20:30:07 +0530

    Added category Page

---
+++ frontend/src/pages/CategoryPage/CategoryPage.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../../components/FoodItem/FoodItem'
+
+const CategoryPage = () => {
+    const { categoryName } = useParams(); // URL se "Rolls" nikalega
+    const { food_list } = useContext(StoreContext);
+
+    return (
+        <div className='category-page'>
+            <h1>{categoryName} Special</h1>
+            <div className='category-list'>
+                {food_list.map((item, index) => {
+                    if (categoryName === item.category) {
+                        return <FoodItem key={index} {...item} />
+                    }
+                    return null;
+                })}
+            </div>
+        </div>
+    )
+}
+export default CategoryPage;
